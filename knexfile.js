504bd799ffb4e9d1eebb87cf0454afbeed841878
stoic-plus <ricardoledesmadev@gmail.com>
2019-03-19 17:50:27 -0600

    adds production configuration for Knex

--- knexfile.js
+++ knexfile.js
@@ -9,5 +9,16 @@ module.exports = {
     seeds: {
       directory: './db/seeds/dev'
     }
+  },
+  production: {
+    client: 'pg',
+    connection: 'postgres://kclgznvrbevnhw:8a1a11ba180e99b9791379e4f530e6db0f309b0256f5fd6b542dcaa173c51bae@ec2-54-197-232-203.compute-1.amazonaws.com:5432/d97o2lvbdi0f6l',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/prod'
+    }
   }
 }
