0a0b4a967a5179678a0895d3946da9d515668797
stoic-plus <ricardoledesmadev@gmail.com>
2019-03-21 13:01:40 -0600

    adds cors - only allowing FE origin (github.io)

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
+const corsOptions = {
+  origin: 'https://maddyg91.github.io',
+  optionsSuccessStatus: 200
+}
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'PlayPlay';
 
-app.get('/', (request, response) => {
+app.get('/', cors(corsOptions), (request, response) => {
   response.send('Welcome To Play Play');
 });
 
-app.get('/api/v1/favorites', (request, response) => {
+app.get('/api/v1/favorites', cors(corsOptions), (request, response) => {
   database('songs').select()
     .then((songs) => {
       response.status(200).json(songs);
