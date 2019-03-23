b9088941f3e96d3299e71a605954adadd57c1292
Maddie Jones <maddyg91@gmail.com>
2019-03-22 20:29:49 -0600

    Adds test for get favorite by id

--- test/endpoints.spec.js
+++ test/endpoints.spec.js
@@ -80,10 +80,50 @@ describe('API routes', function(){
           done();
         });
     });
+
     it('returns 400 error if favorite not found', function(done){
       chai.request(server)
         .put('/api/v1/favorites/20')
         .send({ name: "new_song", artist_name: "new_artist" })
+        .end((err, response) => {
+          response.should.have.status(400);
+          response.body.should.have.property('message');
+          response.body.message.should.equal('favorite not found');
+          done();
+      });
+    });
+  });
+
+  describe('GET /api/v1/favorites/:id ', function(){
+    it('it returns favorite by id', function(done){
+      chai.request(server)
+        .get('/api/v1/favorites/20')
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.have.property('favorites');
+
+          response.body.favorites.should.have.property('id');
+          response.body.favorites.id.should.equal(20);
+
+          response.body.favorites.should.have.property('name');
+          response.body.favorites.name.should.equal('funk');
+
+          response.body.favorites.should.have.property('artist_name');
+          response.body.favorites.artist_name.should.equal('pink');
+
+          response.body.favorites.should.have.property('genre');
+          response.body.favorites.genre.should.equal('Rock');
+
+          response.body.favorites.should.have.property('rating');
+          response.body.favorites.rating.should.equal('100');
+          done();
+        });
+    });
+
+    it('returns 400 error if favorite with id not found', function(done){
+      chai.request(server)
+        .get('/api/v1/favorites/20')
         .end((err, response) => {
           response.should.have.status(400);
           response.body.should.have.property('message');
